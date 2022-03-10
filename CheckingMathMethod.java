cd8167e32787b83e82de7fd74759b8f1232d385d
AravindVegi <101159327+AravindVegi@users.noreply.github.com>
2022-03-10 08:14:48 +0530

    Add files via upload

---
+++ CheckingMathMethod.java
@@ -0,0 +1,22 @@
+package math;
+
+public class CheckingMathMethod {
+
+	public static void main(String[] args) {
+
+		        int number= 109;
+		        float number1 = 101.6f;
+		        System.out.println(Math.abs(number1));
+		        System.out.println(Math.ceil(number1));
+		        System.out.println(Math.floor(number1));
+		        System.out.println(Math.max(number, number1));
+		        System.out.println(Math.min(number, number1));
+		        System.out.println(Math.log(number1));
+		        System.out.println(Math.exp(number1));
+		        System.out.println(Math.log10(number1));
+		        System.out.println(Math.multiplyFull(number, number));
+		    }
+		
+		 
+	}
+
