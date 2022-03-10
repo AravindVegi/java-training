f396057003387ed5feb78b083d289064b7886778
AravindVegi <101159327+AravindVegi@users.noreply.github.com>
2022-03-10 08:58:13 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,14 @@
+package printName;
+
+public class Method {
+
+	public static void main(String[] args) {
+		
+    name();
+	}
+	public static void name()
+	{
+		System.out.println("Aravind");
+	}
+
+}
